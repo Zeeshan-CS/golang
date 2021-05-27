dc2fdf65db82066e970917241cdd7fa82fbcf9df
Zeeshan-CS <zeshanishfaq1@gmail.com>
2021-05-27 12:32:50 +0500

    iniit commiit

---
+++ sum.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() 
+{
+	/* This is my first sample program. */
+	var x , y ,	 z int
+	
+	fmt.Println("Enter Value of x")
+	fmt.Scanln(&x)
+	
+	
+	fmt.Println("Enter Value of y")
+	fmt.Scanln(&y)
+	
+	z = x + y
+	fmt.Println("Sum of x and y = " , z)
+
+}
